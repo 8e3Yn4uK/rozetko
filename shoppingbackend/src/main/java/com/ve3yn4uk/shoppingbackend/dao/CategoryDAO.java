49665915fa0ea510d7a1f886fbca9b656e427cf4
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-26 01:52:38 +0200

    dto, dao

---
+++ shoppingbackend/src/main/java/com/ve3yn4uk/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,41 @@
+package com.ve3yn4uk.shoppingbackend.dao;
+
+import com.ve3yn4uk.shoppingbackend.dto.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 8e3Yn4uK on 26.04.2019
+ */
+
+public class CategoryDAO implements ICategoryDAO {
+
+    private static List<Category> categories = new ArrayList<>();
+
+        static {
+            Category category = new Category();
+            category.setId(1);
+            category.setName("Television");
+            category.setDescription("Some description for TV");
+            category.setImageURL("CAT_1.png");
+            categories.add(category);
+            Category category2 = new Category();
+            category2.setId(2);
+            category2.setName("Mobile");
+            category2.setDescription("Some description for Mobile");
+            category2.setImageURL("CAT_2.png");
+            categories.add(category2);
+            Category category3 = new Category();
+            category3.setId(2);
+            category3.setName("Laptop");
+            category3.setDescription("Some description for Laptop");
+            category3.setImageURL("CAT_3.png");
+            categories.add(category3);
+        }
+
+    @Override
+    public List<Category> findAll() {
+        return categories;
+    }
+}
---
+++ shoppingbackend/src/main/java/com/ve3yn4uk/shoppingbackend/dao/ICategoryDAO.java
@@ -0,0 +1,15 @@
+package com.ve3yn4uk.shoppingbackend.dao;
+
+import com.ve3yn4uk.shoppingbackend.dto.Category;
+
+import java.util.List;
+
+/**
+ * Created by 8e3Yn4uK on 26.04.2019
+ */
+
+public interface ICategoryDAO {
+
+    List<Category> findAll();
+
+}
