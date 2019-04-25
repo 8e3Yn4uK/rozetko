0666f9998798367bc9dda415264d32205b794aca
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-25 13:18:49 +0200

    creating masterPage

--- onlineshopping/src/main/java/com.ve3yn4uk.onlineshopping.controller/PageController.java
+++ onlineshopping/src/main/java/com.ve3yn4uk.onlineshopping.controller/PageController.java
@@ -13,9 +13,25 @@ public class PageController {
 
     @RequestMapping(value = {"/", "/home", "/index"})
     public ModelAndView index() {
-
         ModelAndView mv = new ModelAndView("page");
-        mv.addObject("hello", "Welcome to Spring Web MVC");
+        mv.addObject("title", "Home");
+        mv.addObject("userClickHome", true);
+        return mv;
+    }
+
+    @RequestMapping(value = "/about")
+    public ModelAndView about() {
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", "About Us");
+        mv.addObject("userClickAbout", true);
+        return mv;
+    }
+
+    @RequestMapping(value = "/contact")
+    public ModelAndView contact() {
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", "Contact Us");
+        mv.addObject("userClickContact", true);
         return mv;
     }
 
